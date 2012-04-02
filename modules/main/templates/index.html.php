a1311135a57416d116720110e15d5ea93d96f2ce
Daniel André Eikeland <zegenie@gmail.com>
2012-04-02 16:57:31 +0200

    better frontpage

--- modules/main/templates/index.html.php
+++ modules/main/templates/index.html.php
@@ -3,29 +3,35 @@
 	<?php echo image_tag('/images/banner.jpg'); ?>
 	<?php //<div style="text-align: center; position: absolute; top: 90px; font-size: 1.3em; left: 400px; color: #AAA;">Banner goes here</div> ?>
 </div>
-<div class="content game-intro" style="padding-top: 20px; padding-bottom: 20px;">
-	<b>Dragon Evo: The Card Game</b> is a unique online card game experience inspired by the Dragon Evo fantasy universe.<br>
-	<br>
-	Build a powerful deck with the over 50 possible cards and countless variations available. Increase your powers by levelling up cards, attacks and abilities to become a fearless adversary. Combine elements, powers and magic, and choose from a variety of strategies to achieve domination.<br>
-	<br>
-	<b>Dragon Evo: The Card Game</b> is unique, online trading card game - a one-of-a kind experience. Not a game of luck or dices, but one of tactics, timing and skill.<br>
-</div>
-<div class="content">
-	<div class="element-badge gold">
-		<?php echo image_tag('/images/icon-gold.png', array('style' => 'height: 50px; margin-right: 0;')); ?>
-		<div>Lots of GOLD!</div>
+<div class="pergament">
+	<div class="content game-intro" style="padding-top: 20px; padding-bottom: 20px;">
+		<b>Dragon Evo: The Card Game</b> is a unique online card game experience inspired by the Dragon Evo fantasy universe.<br>
+		<br>
+		Build a powerful deck with the over 50 possible cards and countless variations available. Increase your powers by levelling up cards, attacks and abilities to become a fearless adversary. Combine elements, powers and magic, and choose from a variety of strategies to achieve domination.<br>
+		<br>
+		<b>Dragon Evo: The Card Game</b> is unique, online trading card game - a one-of-a kind experience. Not a game of luck or dices, but one of tactics, timing and skill.<br>
 	</div>
-	<div class="element-badge magic">
-		<?php echo image_tag('/images/magic_small.png', array('style' => 'margin-right: 0;')); ?>
-		<div>Master the art of magic!</div>
+	<div class="content">
+		<div class="element-badge gold">
+			<?php echo image_tag('/images/icon-gold.png', array('style' => 'height: 50px; margin-right: 0;')); ?>
+			<div>Lots of GOLD!</div>
+		</div>
+		<div class="element-badge magic">
+			<?php echo image_tag('/images/magic_small.png', array('style' => 'margin-right: 0;')); ?>
+			<div>Master the art of magic!</div>
+		</div>
+		<div class="element-badge elements">
+			<?php echo image_tag('/images/attack_air.png', array('style' => 'float: none;')); ?>
+			<?php echo image_tag('/images/attack_dark.png', array('style' => 'float: none;')); ?>
+			<?php echo image_tag('/images/attack_earth.png', array('style' => 'float: none;')); ?>
+			<?php echo image_tag('/images/attack_fire.png', array('style' => 'float: none;')); ?>
+			<?php echo image_tag('/images/attack_freeze.png', array('style' => 'float: none;')); ?>
+			<?php echo image_tag('/images/attack_poison.png', array('style' => 'float: none;')); ?>
+			<div style="margin-top: 0px;">Conquer the elements</div>
+		</div>
 	</div>
-	<div class="element-badge elements">
-		<?php echo image_tag('/images/attack_air.png', array('style' => 'float: none;')); ?>
-		<?php echo image_tag('/images/attack_dark.png', array('style' => 'float: none;')); ?>
-		<?php echo image_tag('/images/attack_earth.png', array('style' => 'float: none;')); ?>
-		<?php echo image_tag('/images/attack_fire.png', array('style' => 'float: none;')); ?>
-		<?php echo image_tag('/images/attack_freeze.png', array('style' => 'float: none;')); ?>
-		<?php echo image_tag('/images/attack_poison.png', array('style' => 'float: none;')); ?>
-		<div style="margin-top: 0px;">Conquer the elements</div>
+	<br style="clear: both;">
+	<div class="content game-intro" style="margin-top: 10px; padding-bottom: 20px;">
+		<b>Dragon Evo: The Card Game</b> is currently in development. If you want to join in on the closed alpha-testing when it starts, check out our <a href="https://www.facebook.com/pages/Dragon-Evo-The-card-game/181442641866848">facebook-page</a>!
 	</div>
 </div>
