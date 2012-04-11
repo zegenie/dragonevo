261f979b6530d4e70f8589a6d93c12a1dd248d59
Daniel André Eikeland <zegenie@gmail.com>
2012-04-11 16:29:08 +0200

    layout update

--- modules/main/templates/index.html.php
+++ modules/main/templates/index.html.php
@@ -1,17 +1,28 @@
-<?php $csp_response->setTitle(__('Frontpage')); ?>
-<div id="frontpage-banner">
+<?php $csp_response->setTitle(__('Dragon Evo - The Card Game')); ?>
+<?php /*<div id="frontpage-banner">
 	<?php echo image_tag('/images/banner.jpg'); ?>
 	<?php //<div style="text-align: center; position: absolute; top: 90px; font-size: 1.3em; left: 400px; color: #AAA;">Banner goes here</div> ?>
-</div>
-<div class="pergament">
-	<div class="content game-intro" style="padding-top: 20px; padding-bottom: 20px;">
-		<b>Dragon Evo: The Card Game</b> is a unique online card game experience inspired by the Dragon Evo fantasy universe.<br>
+</div> */ ?>
+<div class="pergament" style="margin-top: 70px; padding-top: 30px;">
+	<ul class="cards" style="margin-left: 100px;">
+		<li class="card flipped" id="card-1"></li>
+		<li class="card flipped" id="card-2"></li>
+		<li class="card flipped" id="card-3"></li>
+	</ul>
+	<br style="clear: both;">
+	<div class="content game-intro" style="padding-top: 50px; padding-bottom: 30px;">
+		<b>Dragon Evo: The Card Game</b> is a unique online card game experience inspired by, and set in the Dragon Evo fantasy universe. Build a powerful deck with countless cards and variations available. Increase your powers by levelling up cards, attacks and abilities to become a fearless adversary. Combine elements, powers and magic, and choose from a variety of strategies to achieve domination.<br>
 		<br>
-		Build a powerful deck with the over 50 possible cards and countless variations available. Increase your powers by levelling up cards, attacks and abilities to become a fearless adversary. Combine elements, powers and magic, and choose from a variety of strategies to achieve domination.<br>
+		<b>Play it wherever you want, on whatever device you want</b><br>
+		Combine phones, tablets, iPads, laptops and computers for a one-of-a kind experience, bringing out the best of each device.<br>
 		<br>
-		<b>Dragon Evo: The Card Game</b> is unique, online trading card game - a one-of-a kind experience. Not a game of luck or dices, but one of tactics, timing and skill.<br>
+		<b>Dragon Evo: The Card Game</b> is unique, online trading card game - not a game of luck or dices, but one of tactics, timing and skill.<br>
+		<br>
+		<p class="smaller">
+			<b>Dragon Evo: The Card Game</b> is currently in development. If you want to join in on the closed alpha-testing when it starts, check out our <a href="https://www.facebook.com/pages/Dragon-Evo-The-card-game/181442641866848"><b>Facebook-page</b></a>!
+		</p>
 	</div>
-	<div class="content">
+	<?php /*<div class="content">
 		<div class="element-badge gold">
 			<?php echo image_tag('/images/icon-gold.png', array('style' => 'height: 50px; margin-right: 0;')); ?>
 			<div>Lots of GOLD!</div>
@@ -29,9 +40,5 @@
 			<?php echo image_tag('/images/attack_poison.png', array('style' => 'float: none;')); ?>
 			<div style="margin-top: 0px;">Conquer the elements</div>
 		</div>
-	</div>
-	<br style="clear: both;">
-	<div class="content game-intro" style="margin-top: 10px; padding-bottom: 20px;">
-		<b>Dragon Evo: The Card Game</b> is currently in development. If you want to join in on the closed alpha-testing when it starts, check out our <a href="https://www.facebook.com/pages/Dragon-Evo-The-card-game/181442641866848">facebook-page</a>!
-	</div>
+	</div> */ ?>
 </div>
